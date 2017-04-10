a05ec6506dba08a7a6bd4380e700027774f76e71
Josh Maloney <Josh@Joshs-MacBook-Pro-2.local>
2017-04-10 12:23:18 +0930

    Tables correctly displaying. Need to make them prettier as well as dry out my code

--- web/app/helpers/line_scores_helper.rb
+++ web/app/helpers/line_scores_helper.rb
@@ -1,2 +1,14 @@
 module LineScoresHelper
+  def home_name(game)
+    game["game"]["home"]["name"]
+  end
+
+  def away_name(game)
+    game["game"]["away"]["name"]
+  end
+  
+  def innings(game, homeaway)
+    game["game"][homeaway]["scoring"]
+  end
+
 end
---
+++ web/app/models/line_scores.rb
@@ -0,0 +1,16 @@
+require 'json'
+require 'net/http'
+
+class LineScores
+  def index
+    url = URI.parse("http://api.sportradar.us/mlb-t6/games/2017/04/07/boxscore.json?api_key=mhp4jgscq9qpbq9a9et3k3bv")
+  	req = Net::HTTP::Get.new(url.to_s)
+  	res = Net::HTTP.start(url.host, url.port) {|http|
+  		http.request(req)
+  	}
+
+  	@output = JSON.parse(res.body)
+    @output = @output["league"]["games"]
+    # puts JSON.pretty_generate(@output)
+  end
+end
