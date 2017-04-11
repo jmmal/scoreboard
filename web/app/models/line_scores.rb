22064178c34241c9345205b06ccc09b520d108dc
Josh Maloney <Josh@Joshs-MacBook-Pro-2.local>
2017-04-11 14:22:35 +0930

    Removed API keys from controller and now storing them in secrets file, scoreboard working for delayed and in progress games

--- web/app/models/line_scores.rb
+++ web/app/models/line_scores.rb
@@ -3,15 +3,12 @@ require 'net/http'
 
 class LineScores
   def index(date)
-    url = URI.parse("http://api.sportradar.us/mlb-t6/games/#{date}/boxscore.json?api_key=mhp4jgscq9qpbq9a9et3k3bv")
+    url = URI.parse("http://api.sportradar.us/mlb-t6/games/#{date}/boxscore.json?api_key=#{ENV['SPORTSRADAR_API_KEY']}")
   	req = Net::HTTP::Get.new(url.to_s)
   	res = Net::HTTP.start(url.host, url.port) {|http|
   		http.request(req)
   	}
-
   	@output = JSON.parse(res.body)
-    puts JSON.pretty_generate(@output)
     @output = @output["league"]["games"]
-
   end
 end
